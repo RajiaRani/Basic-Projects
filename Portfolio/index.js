e67d6cc8d7045c5ab683282073179731942f9438
Rajia Rani <rajiarani.syal@gmail.com>
2024-07-09 22:03:43 -0500

    added

--- Portfolio/index.js
+++ Portfolio/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const  { v4 : uuidv4 } = require("uuid");
 
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -10,6 +11,7 @@ app.set("views", path.join(__dirname, "views"));
 
 let info = [
     {
+        id : uuidv4(),
         name: "Rajia Rani",
         age : 28,
        email: "rajia@gmail.com",
@@ -17,30 +19,29 @@ let info = [
     }
 ];
 let exp = [
-        {   
+        {  
+            id : uuidv4(), 
             year: "5 years",
             profile:"Assistant Professor ",
             area : "Chemistry"
-        },
-        {   
-            year: "3 years",
-            profile:"Assistant Professor ",
-            area : "Chemistry"
-        },
+        }
        ];
 
 let edu = [
         {
+            id : uuidv4(),
             deg:"MSc (Chemistry)",
             clg:"Lovely Professional University (Punjab, India)",
             marks:"70%",
         },
         {
+            id : uuidv4(),
             deg:"BSc(Non-Medical)",
             clg:"PCMD college for women (Punjab, India)",
             marks:"75%",
         },
         {
+            id : uuidv4(),
             deg:"12",
             clg:"Sr.Sec.Govt.Girls School Nehru Garden (Punjab)",
             marks:"78%",
@@ -50,6 +51,7 @@ let edu = [
 
 let skills = [
     {
+        id : uuidv4(),
         skill1: "html",
         skill2:"css",
         skill3: "javaScript",
@@ -64,6 +66,16 @@ let skills = [
 app.get("/portfolio", (req,res) => {
     res.render("index.ejs", {info, edu, exp, skills});
 });
+
+// Education Route
+// Show Route
+app.get ("/portfolio/:id/show", (req,res) => {
+    let { id } = req.params;
+    let Ed = edu.find((e) => id === e.id);
+    res.render("edushow.ejs");
+});
+
+
 
 
 app.listen(8080, () => {
